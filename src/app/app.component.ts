47401e83c32f6c2d5c630f5619b477ce6f61785b
aguessous <37543961+aguessous@users.noreply.github.com>
2019-08-13 20:54:38 -0400

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
+import { SomeComponent } from './some/some.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-bootstrapModal';
+  
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+
+  openModal(){
+    this.modalRef = this.modalService.show(SomeComponent);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
+import { Some.componentComponent } from './some.component/some.component.component';
+import { SomeComponent } from './some/some.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      Some.componentComponent,
+      SomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      ModalModule.forRoot()
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
